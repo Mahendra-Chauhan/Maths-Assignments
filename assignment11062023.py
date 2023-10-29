15c976227ef2b23551fa6cca4e38c28eccc6eaf0
Mahendra Chauhan <144583394+Mahendrathemaster@users.noreply.github.com>
2023-10-29 12:54:27 +0530

    Add files via upload

---
+++ assignment11062023.py
@@ -0,0 +1,20 @@
+#Assignment 1 : Calculate the area and circumference when radius of circle is given.
+#Assignment 2 : Calcualte the perimeter of a traingle when 3 sides are given
+#Stages : Input, Process, Output
+
+#Assignment1 Solution is below:
+radius_circle = 5
+area_circle =3.14*radius_circle**2
+circumference_circle = 2*3.14*radius_circle
+
+print(f"area and circumfrence of circle is =,{area_circle} and {circumference_circle:.2f}")
+#Answer is: area and circumfrence of circle is =,78.5 and 31.40
+
+#Assignment2
+sideA_traingle = 5
+sideB_traingle = 6
+sideC_traingle = 7
+perimeter_of_traingle=sideA_traingle+sideB_traingle+sideC_traingle
+
+print(f"Perimeter of Traingle is {perimeter_of_traingle} when the sides are given 5,6 & 7")
+#Answer is here: Perimeter of Traingle is 18 when the sides are given 5,6 & 7
